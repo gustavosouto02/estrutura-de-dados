55ca03515ea8a87db8514ff773f907a559d2b93c
gustavosouto02 <gustavo.pereira@a.ucb.br>
2024-04-09 11:30:26 -0300

    add atividade3

--- atividade1/atividade1.c
+++ atividade1/atividade1.c
@@ -3,43 +3,50 @@
 
 int main(int argc, char* argv[]){
     
-    int qtd_n = argc -1;
-    int soma_n = 0;
-    float media_n = 0;
-    int menor_n = atoi(argv[1]);
-    int maior_n = atoi(argv[1]);
+    int qtd_n    = argc - 1;
+    int soma_n   = 0;
+    float med_n  = 0;
+    int menor_n  = atoi(argv[1]);
+    int maior_n  = atoi(argv[1]);
+    int vetor_ord_n[qtd_n];
 
     for(int i = 1; i <= qtd_n; i++){
         int n = atoi(argv[i]);
-        soma_n += *argv[i];
+        vetor_ord_n[i-1] = n;
+        soma_n += n;
 
-        if (menor_n > n ){
+        if(menor_n > n){
             menor_n = n;
         }
 
-        if (n > maior_n){
+        if(n > maior_n){
             maior_n = n;
         }
+
     }
 
-    for (int i = 0; i < qtd_n-1; i++){
+    med_n = (float) soma_n / qtd_n;
+
+    printf("Quantidade de numeros recebidos: %d\n", qtd_n);
+    printf("Valor medio dos numeros: %.2f\n", med_n);
+    printf("Menor numero: %d\n", menor_n);
+    printf("Maior numero: %d\n", maior_n);
+
+    for(int i = 0; i < qtd_n-1; i++){
         for(int j = i; j < qtd_n; j++){
             if(vetor_ord_n[i] > vetor_ord_n[j]){
                 int aux = vetor_ord_n[j];
-                vetor_ord_n[j] =  vetor_ord_n[i];
+                vetor_ord_n[j] = vetor_ord_n[i];
                 vetor_ord_n[i] = aux;
             }
-           
         }
     }
     
 
-    media_n = (float) soma_n / qtd_n;
+    for(int i = 0; i < qtd_n; i++){
+        printf("%d ", vetor_ord_n[i]);
+    }
 
-    printf("Quantidade de numeros recebidos: %d\n", qtd_n);
-    printf("Valor medio dos numeros: %.2f\n", media_n);
-    printf("Menor numero: %d\n", menor_n);
-    printf("Maior numero: %d\n", maior_n);
 
     exit(0);
 }
